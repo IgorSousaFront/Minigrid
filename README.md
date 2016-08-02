2f9800d381823556508ac15f45fbff822d693323
Igor <igor.ssqwe@gmail.com>
2016-08-02 18:11:45 -0300

    Iniciando a DEMO

---
+++ README.md
@@ -0,0 +1,25 @@
+# Minigrid demo.
+
+Inicialmente a idéia é bem simples...
+
+### O que preciso pra fazer funcionar?
+
+##### Passo1: 
+* Use a classe `container` para envolver as colunas, como no exemplo...
+
+~~~html
+<section class="container">
+</section>
+~~~
+
+##### Passo 2:
+* Use as classes que deseja para as colunas...
+
+~~~html
+<section class="container">
+    <div class="mb-12 tb-3 dk-4 tv-2">
+</section>
+~~~
+
+##### Passo 3:
+* Seja feliz :D
---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp    = require('gulp'),
+sass        = require('gulp-sass'),
+csso        = require('gulp-csso'),
+concat      = require('gulp-concat'),
+uglify      = require('gulp-uglify'),
+fileinclude = require('gulp-file-include'),
+sync        = require ('browser-sync');
+
+gulp.task('serverRun', function() {
+    sync.init({
+        proxy: "localhost/projetos/minigrid/public/"
+    });
+});
+
+gulp.task('sass', function () {
+  return gulp.src([
+    'assets/sass/main.scss',
+  ])
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('main.css'))
+  .pipe(csso())
+  .pipe(gulp.dest('./public/css/'))
+  .pipe(sync.stream());
+});
+
+gulp.task('js', function () {
+  return gulp.src([
+  ])
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('public/js/'))
+  .pipe(sync.stream());
+});
+
+gulp.task('fileinclude', function() {
+  gulp.src([
+    'index.php'
+  ])
+  .pipe(fileinclude({
+    prefix: '@@',
+    indent: true,
+    basepath: './'
+  }))
+  .pipe(gulp.dest('./public/'))
+  .pipe(sync.stream());
+});
+
+gulp.task('default', ['sass', 'js', 'fileinclude', 'serverRun'], function() {
+  console.log('\n EstÃ¡ trabalhando! \n');
+});
+
+gulp.task('watch', ['default'], function () {
+  gulp.watch('assets/sass/*.scss', ['sass']);
+  gulp.watch('assets/js/*.js', ['js']);
+  gulp.watch('*/*', ['serverRun']);
+  gulp.watch('components/*.php', ['fileinclude']);
+  gulp.watch('*.php', ['fileinclude']);
+});
