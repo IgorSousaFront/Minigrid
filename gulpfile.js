e88729d154eefe3fdc268e4118b9b889ef45a009
Igor <igor.ssqwe@gmail.com>
2016-08-10 14:20:06 -0300

    adicionando classes para offset

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,14 @@ concat      = require('gulp-concat')
 
 gulp.task('sass', function () {
   return gulp.src([
-    'assets/sass/main.scss',
+    'assets/sass/minigrid.scss',
   ])
   .pipe(sass().on('error', sass.logError))
   .pipe(concat('main.css'))
   .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('default', ['sass'], function() {
-  console.log('\n Funcionando! \n');
-});
+gulp.task('default', ['sass']);
 
 gulp.task('watch', ['default'], function () {
   gulp.watch('assets/sass/*.scss', ['sass']);
